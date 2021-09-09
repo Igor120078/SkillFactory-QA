d077feb43791cb36c2cf076f5105675d494f8b81
Igor Ponomarov <absvox@gmail.com>
2021-09-09 09:36:05 +0200

    Test 13.8.19 fixed

--- Test_13_8_19.py
+++ Test_13_8_19.py
@@ -2,31 +2,42 @@
 
 SEP1 = '*' * 50
 SEP2 = '=' * 50
+
+
 def main():
     guests = get_guests_age(get_tickets_number())
     total_price = get_tickets_price(guests)
     print(SEP2)
     print(f"Your total price is: {round(total_price, 2)}$")
 
+
 def get_tickets_number():
-    print(SEP1, 'Welcome to our price calculating system'.center(50), SEP1, sep='\n')
+    print(SEP1, 'Welcome to our price-calculating system'.center(50), SEP1, sep='\n')
     try:
         tickets = int(input("Enter a number of tickets you want to buy: "))
         if tickets <= 0:
             print(SEP2, 'Sorry, but to determine the number of tickets you must enter a positive number.',
-            'See you next time.', sep='\n')
+                  'See you next time.', sep='\n')
             exit()
         print(SEP2)
         return tickets
     except ValueError:
-        print(SEP2, 'You have enter a number! See you next time.', sep='\n')
+        print(SEP2, 'You have enter a whole number! See you next time.', sep='\n')
         exit()
+
 
 def get_guests_age(num):
     guests = {}
-    for n in range(1, num+1):
-        guests['guest_' + str(n)] = int(input(f'Enter the age of the guest #{n}: '))
+    for n in range(1, num + 1):
+        age_ = int(input(f'Enter the age of the guest #{n}: '))
+        if age_ <= 0:
+            print(SEP2, 'Sorry, but to determine the age of the guest you must enter a positive number.',
+                  'See you next time.', sep='\n')
+            exit()
+        guests['guest_' + str(n)] = age_
+
     return guests
+
 
 def get_discount(guests):
     if len(guests) > 3:
@@ -34,6 +45,7 @@ def get_discount(guests):
     else:
         discount = 0
     return discount
+
 
 def get_tickets_price(guests):
     total_price = 0
